e693110ad6d95f3b0c749b7bf75e61c6ba35a49c
rajeshpleti <61886843+rajeshpleti@users.noreply.github.com>
2022-11-13 14:41:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
-streamlit.title('my lord rama')
+import pandas
+streamlit.title('welcome mallika')
 streamlit.header('brekfastmenu')
 streamlit.text('🌕 omega')
 streamlit.text('🥙 kale')
@@ -7,3 +8,7 @@ streamlit.text( '🐔 hard')
 
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+streamlit.dataframe(my_fruit_list)
